4dbf59c9aa671f2d0ed6f7b94d6c606c52b575f1
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2024-02-22 01:42:57 +0530

    weather icon added for different conditions

--- Day02/script.js
+++ Day02/script.js
@@ -1,6 +1,8 @@
 const apiKey = "";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
+//TODO Add Digital time and day and night Icon ,
+
 const citySearch = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -14,7 +16,7 @@ async function checkWeather(city) {
     document.querySelector(".weather").style.display = "none";
   } else {
     var data = await response.json();
-
+    console.log(data);
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + `°C`;
@@ -22,11 +24,19 @@ async function checkWeather(city) {
     document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
 
     // create for multiple conditions
-    //   if (data.weather[0].main == "Clear") {
-    //     weatherIcon.src = "assets/cloudy-day.png";
-    //     // weatherIcon.setAttribute("src", "assets/cloudy-day.png");
-    //     // will work same
-    //   }
+    if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "assets/clear-sky.png";
+      // weatherIcon.setAttribute("src", "assets/cloudy-day.png");
+      // will work same
+    } else if (data.weather[0].main == "Clouds") {
+      weatherIcon.setAttribute("src", "assets/cloudy.png");
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.setAttribute("src", "assets/rain.png");
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.setAttribute("src", "assets/mist.png");
+    } else if (data.weather[0].main == "Haze") {
+      weatherIcon.setAttribute("src", "assets/haze.png");
+    }
     document.querySelector(".error").style.display = "none";
     document.querySelector(".weather").style.display = "block"; // changing the display from none to block when entering city name
   }

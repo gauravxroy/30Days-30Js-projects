1abc0148063bf3272ad922151dda541c42eacf93
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2024-04-11 09:38:53 +0530

    day07

--- Day06/script.js
+++ Day06/script.js
@@ -9,10 +9,9 @@ button.addEventListener("click", function (e) {
   inputBox1.className = "input-box";
   inputBox1.setAttribute("contenteditable", "true");
   delimg.className = "delBtn";
-  delimg.src = "delete.png";
-  //copy Img icon
   copyimg.className = "copyBtn";
   copyimg.src = "copy.png";
+  delimg.src = "delete.png";
   container.appendChild(inputBox1);
   inputBox1.appendChild(copyimg);
   inputBox1.appendChild(delimg);
@@ -20,16 +19,23 @@ button.addEventListener("click", function (e) {
 
 //added confirm delete pop/window whenever clicked on delete icon
 container.addEventListener("click", function (e) {
-  if (e.target.tagName === "IMG" && confirm("Do You Want to delete!") == true) {
+  if (
+    e.target.classList.contains("delBtn") &&
+    confirm("Do You Want to delete!") == true
+  ) {
     e.target.parentElement.remove();
   }
 });
 
 // trying to add copy text functionalities
 
-// container.addEventListener("click", function (e) {
-//   if (e.target.classList === "copyBtn") {
-//     const clipBoard = navigator.clipboard;
-//     clipBoard.writeText(inputBox.innerText);
-//   }
-// });
+container.addEventListener("click", function (e) {
+  if (e.target.classList.contains("copyBtn") == true) {
+    var textToCopy = Array.from(container.childNodes)
+      .filter((node) => node.nodeType === Node.TEXT_NODE)
+      .map((node) => node.textContent.trim())
+      .join(" ");
+    const clipBoard = navigator.clipboard;
+    clipBoard.writeText(textToCopy);
+  }
+});

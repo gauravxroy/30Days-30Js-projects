572105279112c8c15a574122faee0af6f192c4b1
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2024-05-19 23:30:50 +0530

    Day 09 completed

---
+++ Day09/script.js
@@ -0,0 +1,8 @@
+// const API_URL = ``;
+// const data = document.querySelector(".Text");
+// fetch(API_URL)
+//   .then(function (res) {
+//     return res;
+//     console.log(res);
+//   })
+//   .catch();

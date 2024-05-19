85d5bce39644d1e52ab88f864e23880612a58835
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2024-05-19 23:34:56 +0530

    assets

--- Day08/script.js
+++ Day08/script.js
@@ -2,6 +2,7 @@ const API_URL = "https://api.quotable.io/random?maxLength=100";
 const quotes = document.querySelector("#quote");
 const author = document.querySelector("#author");
 const nxtBtn = document.querySelector(".nextBtn");
+const tweetBtn = document.querySelector(".shareBtn");
 async function getQuote(url) {
   try {
     const response = await fetch(url);
@@ -20,6 +21,18 @@ nxtBtn.addEventListener("click", function () {
   getQuote(API_URL);
 });
 
+function tweet() {
+  window.open(
+    //need to fix
+    // `https://twitter.com/intent/tweet?text=${data.content}`,
+
+    `https://twitter.com/intent/tweet?text=` + quotes.innerHTML,
+    "Tweet Quote",
+    "width=800",
+    "height=600"
+  );
+}
+// tweetBtn.addEventListener("click", function (e) {});
 // fetch(API_URL)
 //   .then(function (response) {
 //     return response.json();
